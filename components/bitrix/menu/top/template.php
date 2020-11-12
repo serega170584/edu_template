ddd1bbdb33bf78e5c3d168d15036b4160d6e1c87
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-12 14:42:00 +0300

    add menu

--- components/bitrix/menu/top/template.php
+++ components/bitrix/menu/top/template.php
@@ -11,9 +11,10 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /** @var string $templateFolder */
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
-
+var_dump($arResult);
+die('asd');
 $this->setFrameMode(true);
-if (empty($arResult["ALL_ITEMS"]))
+if (!$arResult["ALL_ITEMS"])
     return;
 ?>
 <ul class="top-menu">
