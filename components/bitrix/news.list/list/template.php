ceb66eea93a098905fe7f1276d0a15fe03de91c7
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-18 21:55:39 +0300

    add slider component

--- components/bitrix/news.list/list/template.php
+++ components/bitrix/news.list/list/template.php
@@ -39,71 +39,91 @@ $this->setFrameMode(true);
                             data-rotate="0" data-saveperformance="off"
                             data-title="unique" data-description="">
                             <!-- MAIN IMAGE -->
-                            <img src="upload/slider/slider-image-1.jpg" alt="" data-bgposition="center center"
+                            <img src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>"
+                                 alt="<?= $arItem["PREVIEW_PICTURE"]["ALT"] ?>" data-bgposition="center center"
                                  data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg" data-no-retina>
+
+                            <!-- LAYERS -->
+                            <!-- LAYER NR. 1 -->
+                            <div class="tp-caption tp-shape tp-shapewrapper tp-resizeme"
+                                 id="slide-<?= $index ?>-layer-1"
+                                 data-x="['left','left','left','left']"
+                                 data-hoffset="['0','0','0','0']"
+                                 data-y="['top','top','top','top']"
+                                 data-voffset="['130','130','130','130']"
+                                 data-width="['530','530','430','420']"
+                                 data-height="330"
+                                 data-whitespace="nowrap"
+                                 data-transform_idle="o:1;"
+                                 data-transform_in="z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;s:1500;e:Power3.easeOut;"
+                                 data-transform_out="x:left;s:1000;e:Power3.easeInOut;s:1000;e:Power3.easeInOut;"
+                                 data-start="500"
+                                 data-responsive_offset="on"
+                                 style="z-index: 5;background-color:rgba(255, 255, 255, 1.00);border-color:rgba(0, 0, 0, 0);">
+                            </div>
+                            <!-- LAYER NR. 2 -->
+                            <div class="tp-caption Woo-TitleLarge tp-resizeme"
+                                 id="slide-<?= $index ?>-layer-2"
+                                 data-x="['left','left','left','left']"
+                                 data-hoffset="['40','40','40','35']"
+                                 data-y="['top','top','top','top']"
+                                 data-voffset="['170','170','170','170']"
+                                 data-width="450"
+                                 data-height="none"
+                                 data-whitespace="normal"
+                                 data-transform_idle="o:1;"
+                                 data-transform_in="z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;s:1500;e:Power3.easeOut;"
+                                 data-transform_out="x:left;s:1000;e:Power3.easeInOut;s:1000;e:Power3.easeInOut;"
+                                 data-start="700"
+                                 data-splitin="none"
+                                 data-splitout="none"
+                                 data-responsive_offset="on"
+                                 style="z-index: 6; min-width: 370px; max-width: 450px; white-space: normal;text-align:left;">
+                                <? echo $arItem["NAME"] ?>
+                            </div>
+                            <!-- LAYER NR. 3 -->
+                            <div class="tp-caption tp-shape tp-shapewrapper tp-line-shape tp-resizeme"
+                                 id="slide-<?= $index ?>-layer-3"
+                                 data-x="['left','left','left','left']"
+                                 data-hoffset="['0','0','0','0']"
+                                 data-y="['top','top','top','top']"
+                                 data-voffset="['165','165','165','165']"
+                                 data-width="['3','3','3','3']"
+                                 data-height="100"
+                                 data-whitespace="nowrap"
+                                 data-transform_idle="o:1;"
+                                 data-transform_in="z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;s:1500;e:Power3.easeOut;"
+                                 data-transform_out="x:left;s:1000;e:Power3.easeInOut;s:1000;e:Power3.easeInOut;"
+                                 data-start="700"
+                                 data-responsive_offset="on"
+                                 style="z-index: 6;">
+                            </div>
+                            <!-- LAYER NR. 4 -->
+                            <div class="tp-caption Woo-Rating tp-resizeme"
+                                 id="slide-<?= $index ?>-layer-4"
+                                 data-x="['left','left','left','left']"
+                                 data-hoffset="['40','40','40','35']"
+                                 data-y="['top','top','top','top']"
+                                 data-voffset="['286','286','286','286']"
+                                 data-width="450"
+                                 data-height="none"
+                                 data-whitespace="normal"
+                                 data-transform_idle="o:1;"
+                                 data-transform_in="z:0;rX:0;rY:0;rZ:0;sX:0.9;sY:0.9;skX:0;skY:0;opacity:0;s:1500;e:Power3.easeOut;"
+                                 data-transform_out="x:left;s:1000;e:Power3.easeInOut;s:1000;e:Power3.easeInOut;"
+                                 data-start="800"
+                                 data-splitin="none"
+                                 data-splitout="none"
+                                 data-responsive_offset="on"
+                                 style="z-index: 8; min-width: 370px; max-width: 450px; white-space: normal; text-align:left;">
+                                <? if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]): ?>
+                                    <? echo $arItem["PREVIEW_TEXT"]; ?>
+                                <? endif; ?>
+                            </div>
                         </li>
                         <?php
                         ++$index;
                         ?>
-                    <p class="news-item" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
-                        <? if ($arParams["DISPLAY_PICTURE"] != "N" && is_array($arItem["PREVIEW_PICTURE"])): ?>
-                            <? if (!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])): ?>
-                                <a href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><img
-                                            class="preview_picture"
-                                            border="0"
-                                            src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>"
-                                            width="<?= $arItem["PREVIEW_PICTURE"]["WIDTH"] ?>"
-                                            height="<?= $arItem["PREVIEW_PICTURE"]["HEIGHT"] ?>"
-                                            alt="<?= $arItem["PREVIEW_PICTURE"]["ALT"] ?>"
-                                            title="<?= $arItem["PREVIEW_PICTURE"]["TITLE"] ?>"
-                                            style="float:left"
-                                    /></a>
-                            <? else: ?>
-                                <img
-                                        class="preview_picture"
-                                        border="0"
-                                        src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>"
-                                        width="<?= $arItem["PREVIEW_PICTURE"]["WIDTH"] ?>"
-                                        height="<?= $arItem["PREVIEW_PICTURE"]["HEIGHT"] ?>"
-                                        alt="<?= $arItem["PREVIEW_PICTURE"]["ALT"] ?>"
-                                        title="<?= $arItem["PREVIEW_PICTURE"]["TITLE"] ?>"
-                                        style="float:left"
-                                />
-                            <? endif; ?>
-                        <? endif ?>
-                        <? if ($arParams["DISPLAY_DATE"] != "N" && $arItem["DISPLAY_ACTIVE_FROM"]): ?>
-                            <span class="news-date-time"><? echo $arItem["DISPLAY_ACTIVE_FROM"] ?></span>
-                        <? endif ?>
-                        <? if ($arParams["DISPLAY_NAME"] != "N" && $arItem["NAME"]): ?>
-                            <? if (!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])): ?>
-                                <a href="<? echo $arItem["DETAIL_PAGE_URL"] ?>"><b><? echo $arItem["NAME"] ?></b></a>
-                                <br/>
-                            <? else: ?>
-                                <b><? echo $arItem["NAME"] ?></b><br/>
-                            <? endif; ?>
-                        <? endif; ?>
-                        <? if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]): ?>
-                            <? echo $arItem["PREVIEW_TEXT"]; ?>
-                        <? endif; ?>
-                        <? if ($arParams["DISPLAY_PICTURE"] != "N" && is_array($arItem["PREVIEW_PICTURE"])): ?>
-                            <div style="clear:both"></div>
-                        <? endif ?>
-                        <? foreach ($arItem["FIELDS"] as $code => $value): ?>
-                            <small>
-                                <?= GetMessage("IBLOCK_FIELD_" . $code) ?>:&nbsp;<?= $value; ?>
-                            </small><br/>
-                        <? endforeach; ?>
-                        <? foreach ($arItem["DISPLAY_PROPERTIES"] as $pid => $arProperty): ?>
-                            <small>
-                                <?= $arProperty["NAME"] ?>:&nbsp;
-                                <? if (is_array($arProperty["DISPLAY_VALUE"])): ?>
-                                    <?= implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]); ?>
-                                <? else: ?>
-                                    <?= $arProperty["DISPLAY_VALUE"]; ?>
-                                <? endif ?>
-                            </small><br/>
-                        <? endforeach; ?>
-                        </p>
                     <? endforeach; ?>
                 </ul>
                 <?php
