2bf1824dfc4b444e62517540a1fc4806f8ad1f59
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-18 16:21:55 +0300

    delete data thumbs

--- components/bitrix/news.list/list/template.php
+++ components/bitrix/news.list/list/template.php
@@ -23,17 +23,28 @@ $this->setFrameMode(true);
                 <?= $arResult["NAV_STRING"] ?><br/>
             <? endif; ?>
             <?php
+            $index = 1;
             if ($arResult["ITEMS"]) {
                 ?>
                 <ul>
-                    <? foreach ($arResult["ITEMS"] as $arItem):?>
+                    <? foreach ($arResult["ITEMS"] as $arItem): ?>
                         <?
                         $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
                         $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
                         ?>
+                        <!-- SLIDE  -->
+                        <li data-index="rs-<?= $index ?>" data-transition="fade" data-slotamount="default"
+                            data-easein="default"
+                            data-easeout="default" data-masterspeed="default"
+                            data-thumb="upload/slider/slider-image-1.jpg" data-rotate="0" data-saveperformance="off"
+                            data-title="unique" data-description="">
+                        </li>
+                        <?php
+                        ++$index;
+                        ?>
                     <p class="news-item" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
-                        <? if ($arParams["DISPLAY_PICTURE"] != "N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-                            <? if (!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
+                        <? if ($arParams["DISPLAY_PICTURE"] != "N" && is_array($arItem["PREVIEW_PICTURE"])): ?>
+                            <? if (!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])): ?>
                                 <a href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><img
                                             class="preview_picture"
                                             border="0"
@@ -44,7 +55,7 @@ $this->setFrameMode(true);
                                             title="<?= $arItem["PREVIEW_PICTURE"]["TITLE"] ?>"
                                             style="float:left"
                                     /></a>
-                            <? else:?>
+                            <? else: ?>
                                 <img
                                         class="preview_picture"
                                         border="0"
@@ -55,42 +66,42 @@ $this->setFrameMode(true);
                                         title="<?= $arItem["PREVIEW_PICTURE"]["TITLE"] ?>"
                                         style="float:left"
                                 />
-                            <?endif; ?>
-                        <?endif ?>
-                        <? if ($arParams["DISPLAY_DATE"] != "N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
+                            <? endif; ?>
+                        <? endif ?>
+                        <? if ($arParams["DISPLAY_DATE"] != "N" && $arItem["DISPLAY_ACTIVE_FROM"]): ?>
                             <span class="news-date-time"><? echo $arItem["DISPLAY_ACTIVE_FROM"] ?></span>
-                        <?endif ?>
-                        <? if ($arParams["DISPLAY_NAME"] != "N" && $arItem["NAME"]):?>
-                            <? if (!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
+                        <? endif ?>
+                        <? if ($arParams["DISPLAY_NAME"] != "N" && $arItem["NAME"]): ?>
+                            <? if (!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])): ?>
                                 <a href="<? echo $arItem["DETAIL_PAGE_URL"] ?>"><b><? echo $arItem["NAME"] ?></b></a>
                                 <br/>
-                            <? else:?>
+                            <? else: ?>
                                 <b><? echo $arItem["NAME"] ?></b><br/>
-                            <?endif; ?>
-                        <?endif; ?>
-                        <? if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]):?>
+                            <? endif; ?>
+                        <? endif; ?>
+                        <? if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]): ?>
                             <? echo $arItem["PREVIEW_TEXT"]; ?>
-                        <?endif; ?>
-                        <? if ($arParams["DISPLAY_PICTURE"] != "N" && is_array($arItem["PREVIEW_PICTURE"])):?>
+                        <? endif; ?>
+                        <? if ($arParams["DISPLAY_PICTURE"] != "N" && is_array($arItem["PREVIEW_PICTURE"])): ?>
                             <div style="clear:both"></div>
-                        <?endif ?>
-                        <? foreach ($arItem["FIELDS"] as $code => $value):?>
+                        <? endif ?>
+                        <? foreach ($arItem["FIELDS"] as $code => $value): ?>
                             <small>
                                 <?= GetMessage("IBLOCK_FIELD_" . $code) ?>:&nbsp;<?= $value; ?>
                             </small><br/>
-                        <?endforeach; ?>
-                        <? foreach ($arItem["DISPLAY_PROPERTIES"] as $pid => $arProperty):?>
+                        <? endforeach; ?>
+                        <? foreach ($arItem["DISPLAY_PROPERTIES"] as $pid => $arProperty): ?>
                             <small>
                                 <?= $arProperty["NAME"] ?>:&nbsp;
-                                <? if (is_array($arProperty["DISPLAY_VALUE"])):?>
+                                <? if (is_array($arProperty["DISPLAY_VALUE"])): ?>
                                     <?= implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]); ?>
-                                <? else:?>
+                                <? else: ?>
                                     <?= $arProperty["DISPLAY_VALUE"]; ?>
-                                <?endif ?>
+                                <? endif ?>
                             </small><br/>
-                        <?endforeach; ?>
+                        <? endforeach; ?>
                         </p>
-                    <?endforeach; ?>
+                    <? endforeach; ?>
                 </ul>
                 <?php
             }
