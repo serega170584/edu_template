51da27f06194d94409348fd0b09c8643de8e1ccf
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-19 09:55:19 +0300

    add features html

--- main/index.php
+++ main/index.php
@@ -26,4 +26,54 @@ $APPLICATION->SetTitle("Title");
         "DISPLAY_BOTTOM_PAGER" => "",
     )
 ); ?>
+    <!-- feature-section
+                ================================================== -->
+    <section class="feature-section">
+        <div class="container">
+            <div class="feature-box">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="feature-post">
+                            <div class="icon-holder">
+                                <i class="fa fa-umbrella"></i>
+                            </div>
+                            <div class="feature-content">
+                                <h2>
+                                    Online Learn Courses Management
+                                </h2>
+                                <p>Analyzing negative materials about your brand and addressing them with sentiment analysis and press.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="feature-post">
+                            <div class="icon-holder color2">
+                                <i class="fa fa-id-card-o"></i>
+                            </div>
+                            <div class="feature-content">
+                                <h2>
+                                    Learn from the masters of the field online
+                                </h2>
+                                <p>Analyzing negative materials about your brand and addressing them with sentiment analysis and press.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="feature-post">
+                            <div class="icon-holder color3">
+                                <i class="fa fa-handshake-o"></i>
+                            </div>
+                            <div class="feature-content">
+                                <h2>
+                                    An Introduction-Skills For Learners
+                                </h2>
+                                <p>Analyzing negative materials about your brand and addressing them with sentiment analysis and press.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End feature section -->
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
