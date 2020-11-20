4fb130570d95563838a60c782e40743e9b5a2f9d
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-20 11:27:52 +0300

    add news block

--- main/news/index.php
+++ main/news/index.php
@@ -66,7 +66,6 @@ $APPLICATION->SetTitle("Новости");
                             "DISPLAY_BOTTOM_PAGER" => "",
                         )
                     ); ?>
-                    ; ?>
                 </div>
 
             </div>
