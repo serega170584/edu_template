922fb67918a8d2603c958434472cbe0af520fff7
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-20 11:27:02 +0300

    add news block

--- components/bitrix/news.list/news/template.php
+++ components/bitrix/news.list/news/template.php
@@ -11,7 +11,6 @@
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
-die('asd');
 ?>
 <div class="blog-box">
     <? if ($arParams["DISPLAY_TOP_PAGER"]): ?>
