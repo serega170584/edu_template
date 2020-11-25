37c5607ce0c5930ba3eb71969f271f6b846a7981
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-25 15:10:39 +0300

    add properties to education table

--- components/bitrix/news.list/professions/template.php
+++ components/bitrix/news.list/professions/template.php
@@ -22,67 +22,67 @@ $this->setFrameMode(true);
         </tr>
         <tr>
             <th scope="row">Сроки обучения</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_PERIOD_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Срок действия гос. аккредитации</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_ACCREDITATION_PERIOD_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Уровень образования</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_LEVEL_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Код специальности, направления подготовки</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_CODE_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Описание образовательной программы</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_DESCRIPTION_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Учебный план</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_PLAN_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Аннотации к рабочим программам дисциплин</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_ANNOTATIONS_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Календарный учебный график</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_SCHEDULE_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Методические и иные документы</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_METHODOLOGICAL_DOCUMENTS_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Практики</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_PRACTICES_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Численность лиц, обучающихся за счет бюджета</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_BUDGET_COUNT_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Численность лиц, находящихся на платном обучении</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_PAYED_COUNT_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Языки, на которых происходит обучение</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_LANGUAGES_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Научно-исследовательская работа</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_RESEARCHES_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Сведения о результатах приема</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_RESULTS_VALUE'] ?></td>
         </tr>
         <tr>
             <th scope="row">Результаты перевода и отчисления</th>
-            <td><?= $arItem['PROPERTY_FORM_OF_EDUCATION_VALUE'] ?></td>
+            <td><?= $arItem['PROPERTY_REPLACED_RESULTS_VALUE'] ?></td>
         </tr>
         </tbody>
     </table>
