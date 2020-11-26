5f30799c03ddfa601896c97b036a42416bf0843a
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-26 17:17:59 +0300

    add for applicant page

---
+++ main/main_information/.left.menu.php
@@ -0,0 +1,88 @@
+<?php
+$aMenuLinks = [
+    [
+        "Основные сведения",
+        "/local/templates/edu_template/main/main_information/",
+        [],
+        [],
+        ""
+    ],
+    [
+        "Структура и органы управления образовательной организацией",
+        "/local/templates/edu_template/main/main_information/branches/",
+        [],
+        [],
+        ""
+    ],
+    [
+        "Документы",
+        "/local/templates/edu_template/main/main_information/documents/",
+        [],
+        [],
+        ""
+    ],
+    [
+        "Образование",
+        "/local/templates/edu_template/main/main_information/education/",
+        [],
+        [],
+        ""
+    ],
+    [
+        "Образовательные стандарты РФ",
+        "/local/templates/edu_template/main/main_information/educational_standards/",
+        [],
+        [],
+        ""
+    ],
+    [
+        "Руководство. Педагогический (научно-педагогический) состав",
+        "/local/templates/edu_template/main/main_information/leadership/",
+        [],
+        [],
+        ""
+    ],
+    [
+        "Материально-техническое обеспечение и оснащённость образовательного процесса",
+        "/local/templates/edu_template/main/main_information/provision/",
+        [],
+        [],
+        ""
+    ],
+    [
+        "Стипендии и иные виды материальной поддержки",
+        "/local/templates/edu_template/main/main_information/grants/",
+        [],
+        [],
+        ""
+    ],
+    [
+        "Платные образовательные услуги",
+        "/local/templates/edu_template/main/main_information/services/",
+        [],
+        [],
+        ""
+    ],
+    [
+        "Финансово-хозяйственная деятельность",
+        "/local/templates/edu_template/main/main_information/financialactivity/",
+        [],
+        [],
+        ""
+    ],
+    [
+        "Вакантные места для приема(перевода)",
+        "/local/templates/edu_template/main/main_information/vacancy/",
+        [],
+        [],
+        ""
+    ],
+    [
+        "Абитуриенту",
+        "/local/templates/edu_template/main/for_applicant/",
+        [],
+        [],
+        ""
+    ]
+];
+?>
