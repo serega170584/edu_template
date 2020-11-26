9acfcc3176ac464a9e08f25138692fd4fcf9c8ad
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-26 14:40:18 +0300

    add for applicant page

--- components/bitrix/news.list/programs/template.php
+++ components/bitrix/news.list/programs/template.php
@@ -26,6 +26,8 @@ if ($arResult["ITEMS"]) {
         <? endif; ?>
         <? foreach ($arResult["ITEMS"] as $arItem): ?>
             <?
+        var_dump($arItem);
+        die('asd');
             $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
             $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
             ?>
