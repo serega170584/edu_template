efee5cb9ca07cc5c12da01ff9bd89efeeb6b633c
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-26 14:03:22 +0300

    add vacancy page

---
+++ components/bitrix/news.list/programs/template.php
@@ -0,0 +1,51 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="blog-box">
+    <? if ($arParams["DISPLAY_TOP_PAGER"]): ?>
+        <?= $arResult["NAV_STRING"] ?><br/>
+    <? endif; ?>
+    <? foreach ($arResult["ITEMS"] as $arItem): ?>
+        <?
+        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+        ?>
+        <div class="blog-post list-style">
+            <a href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><img
+                        class="preview_picture"
+                        border="0"
+                        src="<?= $arItem["DETAIL_PICTURE"]["SRC"] ?>"
+                        width="318"
+                        height="227"
+                        alt="<?= $arItem["DETAIL_PICTURE"]["ALT"] ?>"
+                        title="<?= $arItem["DETAIL_PICTURE"]["TITLE"] ?>"
+                        style="float:left"
+                /></a>
+            <div class="post-content">
+                <h2><a href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><?= $arItem["NAME"] ?></a></h2>
+                <p><? if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]): ?>
+                        <? echo $arItem["PREVIEW_TEXT"]; ?>
+                    <? endif; ?></p>
+                <div class="post-meta date">
+                    <? if ($arParams["DISPLAY_DATE"] != "N" && $arItem["DISPLAY_ACTIVE_FROM"]): ?>
+                        <div class="post-meta date"><? echo $arItem["DISPLAY_ACTIVE_FROM"] ?></div>
+                    <? endif ?>
+                </div>
+            </div>
+        </div>
+    <? endforeach; ?>
+    <? if ($arParams["DISPLAY_BOTTOM_PAGER"]): ?>
+        <br/><?= $arResult["NAV_STRING"] ?>
+    <? endif; ?>
+</div>
---
+++ components/bitrix/news/programs/detail.php
@@ -0,0 +1,49 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+$APPLICATION->IncludeComponent("bitrix:news.detail", "", array(
+        "DISPLAY_DATE" => "Y",
+        "DISPLAY_NAME" => "Y",
+        "DISPLAY_PICTURE" => "Y",
+        "DISPLAY_PREVIEW_TEXT" => "Y",
+        "IBLOCK_TYPE" => "edu",
+        "IBLOCK_ID" => \CIBlock::GetList([
+            'ID' => 'ASC'
+        ], [
+            'TYPE' => 'edu',
+            'CODE' => 'NEWS',
+        ])->Fetch()['ID'],
+        "ELEMENT_ID" => $arResult["VARIABLES"]["ELEMENT_ID"],
+        "ELEMENT_CODE" => "",
+        "FIELD_CODE" => array("ID"),
+        "PROPERTY_CODE" => array("DESCRIPTION"),
+        "SET_TITLE" => "Y",
+        "SET_CANONICAL_URL" => "Y",
+        "SET_BROWSER_TITLE" => "Y",
+        "BROWSER_TITLE" => "-",
+        "SET_META_KEYWORDS" => "Y",
+        "META_KEYWORDS" => "-",
+        "SET_META_DESCRIPTION" => "Y",
+        "META_DESCRIPTION" => "-",
+        "SET_STATUS_404" => "Y",
+        "SET_LAST_MODIFIED" => "Y",
+        "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+        "ADD_SECTIONS_CHAIN" => "Y",
+        "ADD_ELEMENT_CHAIN" => "N",
+        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+        "CACHE_TYPE" => "A",
+        "CACHE_TIME" => "3600",
+        "CACHE_GROUPS" => "Y",
+    )
+); ?>
