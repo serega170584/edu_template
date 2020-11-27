90b580545927ae36a10173bc089e669d9d3ef553
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-27 21:38:11 +0300

    add about left menu

--- components/bitrix/news.list/faculties/template.php
+++ components/bitrix/news.list/faculties/template.php
@@ -38,11 +38,13 @@ $this->setFrameMode(true);
             $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
             $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
             ?>
-            <h2><a href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><?= $arItem["NAME"] ?></a></h2>
-            <p><? if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]): ?>
-                    <? echo $arItem["PREVIEW_TEXT"]; ?>
-                <? endif; ?></p>
-            <a class="button-one" href="<?= $arItem["DETAIL_PAGE_URL"] ?>">Подробнее</a>
+            <div>
+                <h2><a href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><?= $arItem["NAME"] ?></a></h2>
+                <p><? if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]): ?>
+                        <? echo $arItem["PREVIEW_TEXT"]; ?>
+                    <? endif; ?></p>
+                <a class="button-one" href="<?= $arItem["DETAIL_PAGE_URL"] ?>">Подробнее</a>
+            </div>
         <? endforeach; ?>
         <? if ($arParams["DISPLAY_BOTTOM_PAGER"]): ?>
             <br/><?= $arResult["NAV_STRING"] ?>
