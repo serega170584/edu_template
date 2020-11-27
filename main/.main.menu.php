f52f20af30d9a20a5bae8fa6df4d9134396be36c
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-27 19:45:38 +0300

    news refactoring

--- main/.main.menu.php
+++ main/.main.menu.php
@@ -2,7 +2,7 @@
 $aMenuLinks = [
     [
         "О ВУЗе",
-        "/local/templates/edu_template/main/news/",
+        "/local/templates/edu_template/main/about/news/",
         [],
         [],
         ""
