3a3dab56d4366a6672fe708ed9dbaad833e923bb
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-27 21:24:13 +0300

    news refactoring

---
+++ components/bitrix/news.detail/faculties/template.php
@@ -0,0 +1,66 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<h1><?= $arResult["NAME"] ?></h1>
+<p><?= $arResult["PROPERTIES"]["CODE"]["VALUE"] ?></p>
+<p><?= $arResult["DETAIL_TEXT"] ?></p>
+<table class="table table-striped">
+    <thead>
+    <th scope="col">Срок обучения: <?= $arResult['PROPERTIES']['PERIOD']['VALUE'] ?></th>
+    <th scope="col">Форма обучения: <?= $arResult['PROPERTIES']['FORM_OF_EDUCATION']['VALUE'] ?></th>
+    <th scope="col">Количество мест: <?= $arResult['PROPERTIES']['BUDGET_COUNT']['VALUE'] ?>
+        бюджетных, <?= $arResult['PROPERTIES']['PAYED_COUNT']['VALUE'] ?> платных
+    </th>
+    <th scope="col">Стоимость платных мест: <?= $arResult['PROPERTIES']['PRICE']['VALUE'] ?> </th>
+    </thead>
+    <tbody>
+    <tr>
+        <th scope="row">
+            Профили подготовки: <?= $arResult['PROPERTIES']['PREPARATORY_PROFILE']['VALUE'] ?>
+        </th>
+        <td>Ключевые дисциплины: <?= $arResult['PROPERTIES']['PRINCIPAL_SUBJECTS']['VALUE'] ?></td>
+        <td>Вступительные испытания: <?php array_map(function ($id) {
+                printf('%s ', CIBlockElement::GetByID($id)->Fetch()['NAME']);
+            }, $arResult['PROPERTIES']['PRELIMINARY_TESTS']['VALUE']) ?></td>
+        <td>
+        </td>
+    </tr>
+    <tr>
+        <th scope="row" colspan="4">
+            <?php
+            $properties = [
+                "PLAN",
+                "ANNOTATIONS",
+                "SCHEDULE",
+                "PRACTICES",
+                "RESEARCHES",
+                "RESULTS",
+                "REPLACED_RESULTS"
+            ];
+            array_map(function ($code) use ($arResult) {
+                $path = CFile::GetPath($arResult["PROPERTIES"][$code]["VALUE"]);
+                printf('<a href="%s" target="_blank">%s</a><br>', $path, basename($path));
+            }, $properties);
+            $properties = [
+                "METHODOLOGICAL_DOCUMENTS",
+            ];
+            array_map(function ($code) {
+                $path = CFile::GetPath($code);
+                printf('<a href="%s" target="_blank">%s</a><br>', $path, basename($path));
+            }, $arResult["PROPERTIES"]["METHODOLOGICAL_DOCUMENTS"]["VALUE"]);
+            ?>
+        </th>
+    </tr>
+    </tbody>
+</table>
--- main/.main.menu.php
+++ main/.main.menu.php
@@ -2,7 +2,7 @@
 $aMenuLinks = [
     [
         "О ВУЗе",
-        "/local/templates/edu_template/main/about/2news/",
+        "/local/templates/edu_template/main/about/news/",
         [],
         [],
         ""
