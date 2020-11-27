7e15d81336285e62fe48e5074ee5f5492199b60b
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-27 21:44:48 +0300

    add about left menu

--- main/about/index.php
+++ main/about/index.php
@@ -5,7 +5,7 @@
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("О ВУЗе");
 $server = \Bitrix\Main\Application::getInstance()->getContext()->getServer();
-LocalRedirect(sprintf('%s/local/templates/edu_template/main/news/', $server->getHttpHost()));
+LocalRedirect('/local/templates/edu_template/main/news/');
 exit;
 ?>
 
