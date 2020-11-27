f4d7bbbb6c21ca5d8bf766bb9f3cae68e0daab40
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-27 21:29:48 +0300

    add about left menu

---
+++ main/about/.left.menu.php
@@ -0,0 +1,25 @@
+<?php
+$aMenuLinks = [
+    [
+        "Новости",
+        "/local/templates/edu_template/main/about/news/",
+        [],
+        [],
+        ""
+    ],
+    [
+        "Основные сведения",
+        "/local/templates/edu_template/main/main_information/",
+        [],
+        [],
+        ""
+    ],
+    [
+        "Факультеты",
+        "/local/templates/edu_template/main/about/faculties/",
+        [],
+        [],
+        ""
+    ]
+];
+?>
