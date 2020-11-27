85e6a369c6a55190c9bd39b5f47b6846949cc243
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-27 21:35:27 +0300

    add about left menu

--- components/bitrix/news.list/faculties/template.php
+++ components/bitrix/news.list/faculties/template.php
@@ -38,7 +38,7 @@ $this->setFrameMode(true);
             $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
             $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
             ?>
-            <div class="blog-post list-style">
+            <div class="blog-post">
                 <h2><a href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><?= $arItem["NAME"] ?></a></h2>
                 <p><? if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]): ?>
                         <? echo $arItem["PREVIEW_TEXT"]; ?>
