e30f48a7bef21b1cc54e6c891ce68f208238d3c6
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-11-30 10:05:28 +0300

    add departments page

---
+++ components/bitrix/news.list/departments_main/template.php
@@ -0,0 +1,42 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<h1>Кафедры</h1>
+<div class="blog-box">
+    <?php
+    if ($arResult["ITEMS"]) {
+        ?>
+        <? if ($arParams["DISPLAY_TOP_PAGER"]): ?>
+            <?= $arResult["NAV_STRING"] ?><br/>
+        <? endif; ?>
+        <? foreach ($arResult["ITEMS"] as $arItem): ?>
+            <?
+            $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+            $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+            ?>
+            <div class="mb-3">
+                <h2><a href="<?= $arItem["DETAIL_PAGE_URL"] ?>"><?= $arItem["NAME"] ?></a></h2>
+                <p><? if ($arParams["DISPLAY_PREVIEW_TEXT"] != "N" && $arItem["PREVIEW_TEXT"]): ?>
+                        <? echo $arItem["PREVIEW_TEXT"]; ?>
+                    <? endif; ?></p>
+                <a class="button-one" href="<?= $arItem["DETAIL_PAGE_URL"] ?>">Подробнее</a>
+            </div>
+        <? endforeach; ?>
+        <? if ($arParams["DISPLAY_BOTTOM_PAGER"]): ?>
+            <br/><?= $arResult["NAV_STRING"] ?>
+        <? endif; ?>
+        <?php
+    }
+    ?>
+</div>
