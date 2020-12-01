965c155059cf41715a8f7bcda5f6673e5fb26fbd
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-12-01 12:30:12 +0300

    add advertise page

--- components/bitrix/news/advertisement/news.php
+++ components/bitrix/news/advertisement/news.php
@@ -15,7 +15,7 @@ $this->setFrameMode(true);
 
 <?$APPLICATION->IncludeComponent(
     "bitrix:news.list",
-    "faculties",
+    "advertisement",
     Array(
         "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
         "IBLOCK_ID" => $arParams["IBLOCK_ID"],
