d4a8f0cbea6cd257593d558fe4c371162d39d1c3
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-12-01 13:07:32 +0300

    main menu refactoring

---
+++ main/for_applicant/.left.menu.php
@@ -0,0 +1,11 @@
+<?php
+$aMenuLinks = [
+    [
+        "Приемная комиссия",
+        "/local/templates/edu_template/main/main_information/",
+        [],
+        [],
+        ""
+    ],
+];
+?>
---
+++ main/for_applicant/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @var CMain $APPLICATION
+ */
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("О ВУЗе");
+$server = \Bitrix\Main\Application::getInstance()->getContext()->getServer();
+LocalRedirect('/local/templates/edu_template/main/about/news/');
+exit;
+?>
+
+<?php
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
+?>
